35122422ca5fa24a3b4c726244d5f1a8fe362639
Adam Ouellette <adamouellette57@gmail.com>
2018-01-13 18:27:49 -0500

    Setting up circleci and testing

---
+++ cachigo_test.go
@@ -0,0 +1,19 @@
+package cachigo
+
+import "testing"
+
+func TestOpen(t *testing.T) {
+
+}
+
+func TestGet(t *testing.T) {
+
+}
+
+func TestPut(t *testing.T) {
+
+}
+
+func TestDelete(t *testing.T) {
+
+}
