a0f9e10d1a358afe01dbb59902f1565b2a134e21
Adam Ouellette <adamouellette57@gmail.com>
2018-01-12 22:03:16 -0500

    Added Put and encoding in JSON BOO

--- cachigo.go
+++ cachigo.go
@@ -1,6 +1,9 @@
 package cachigo
 
 import (
+	"encoding/json"
+	"errors"
+	"log"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -11,7 +14,13 @@ type CachingService struct {
 	db *bolt.DB
 }
 
-var bucketname = []byte("cachigo")
+var (
+	bucketname = []byte("cachigo")
+
+	//ErrBadValue is an error that should only happen when
+	//there is a bad value trying to be stored.
+	ErrBadValue = errors.New("Bad key")
+)
 
 //Open handles the opening of the bolt instance. Path is the full path to the database file.
 // 0640 opend the file with -rw-r----- permissions
@@ -33,4 +42,23 @@ func Open(path string) (*CachingService, error) {
 			return &CachingService{db: db}, nil
 		}
 	}
+}
+
+//Put inserts a key and a value into the data store
+func (cs *CachingService) Put(key string, value interface{}) error {
+	if value == nil {
+		return ErrBadValue
+	} else {
+		cs.db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket(bucketname)
+			enc, err := json.Marshal(value)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = b.Put([]byte(key), enc)
+			return err
+		})
+	}
+
+	return nil
 }
